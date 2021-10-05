26696dc219ee3a27d5c7d0f85cf22d1b8e23aac0
André Lopes <lopes.andre@outlook.com>
2021-10-05 07:13:26 -0300

    Add advanced widgets with queryset

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -1,6 +1,7 @@
+from re import S
 from django import forms
 from django.forms import widgets
-from .models import Pizza
+from .models import Pizza, Size
 
 # class PizzaForm(forms.Form):
 #     topping1 = forms.CharField(label='Topping 1', max_length=100)
@@ -8,13 +9,13 @@ from .models import Pizza
 #     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')])
 
 class PizzaForm(forms.ModelForm):
+
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
         labels = {
             'topping1': 'Topping 1',
             'topping2': 'Topping 2',
-        }
-        widgets = {
-            'topping1': forms.Textarea,
         }
