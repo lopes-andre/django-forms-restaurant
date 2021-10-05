5238897eda724ad04c93751144f7565a5932eda7
André Lopes <lopes.andre@outlook.com>
2021-10-05 07:03:50 -0300

    Update labels

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -10,3 +10,7 @@ class PizzaForm(forms.ModelForm):
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
+        labels = {
+            'topping1': 'Topping 1',
+            'topping2': 'Topping 2',
+        }
